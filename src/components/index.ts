10b658fb5811ad66f8800b7eb16490a7fa6f6549
Luka <lukamartinovic@gmail.com>
2021-07-14 23:30:50 +0200

    fix: removed bad export from components

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,6 @@ export {default as Layout} from './Layout/Layout';
 export {default as Number} from './Number/Number';
 export {default as DifficultyPicker} from './DifficultyPicker/DifficultyPicker';
 export {default as Numbers} from './Numbers/Numbers';
-export {default as Input} from '../hooks/useGameInput';
 export {default as CurrentNumber} from './CurrentNumber/CurrentNumber';
 export {default as TopBar} from './TopBar/TopBar';
 export {default as Statistics} from './Statistics/Statistics'
